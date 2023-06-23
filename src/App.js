a54b86f1c538f784be8aaf1475d5d7edf5e1fa6b
wenhuixu <wenhuixu2017@outlook.com>
2023-06-23 12:59:14 +0930

    styling the basic css of flashcards

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 const SAMPLE_FLASHCARD = [
   {
     id: 1,
-    question:'Question 1',
+    question:'Question 1: This is the content of question 1',
     answer: 'Answer',
     options:[
       'Answer',
@@ -26,7 +26,95 @@ const SAMPLE_FLASHCARD = [
   },
   {
     id: 2,
-    question:'Question 2',
+    question:'Question 2: this is the content of question 2',
+    answer: 'Answer',
+    options:[
+      'Answer',
+      'Answer 1',
+      'Answer 2',
+      'Answer 3'
+    ]
+  },
+  {
+    id: 3,
+    question:'Question 3: this is the content of question 3',
+    answer: 'Answer',
+    options:[
+      'Answer',
+      'Answer 1',
+      'Answer 2',
+      'Answer 3'
+    ]
+  },
+  {
+    id: 4,
+    question:'Question 4: this is the content of question 4',
+    answer: 'Answer',
+    options:[
+      'Answer',
+      'Answer 1',
+      'Answer 2',
+      'Answer 3'
+    ]
+  },
+  {
+    id: 5,
+    question:'Question 5: this is the content of question 5',
+    answer: 'Answer',
+    options:[
+      'Answer',
+      'Answer 1',
+      'Answer 2',
+      'Answer 3'
+    ]
+  },
+  {
+    id: 6,
+    question:'Question 6: this is the content of question 6',
+    answer: 'Answer',
+    options:[
+      'Answer',
+      'Answer 1',
+      'Answer 2',
+      'Answer 3'
+    ]
+  },
+  {
+    id: 7,
+    question:'Question 7: this is the content of question 7',
+    answer: 'Answer',
+    options:[
+      'Answer',
+      'Answer 1',
+      'Answer 2',
+      'Answer 3'
+    ]
+  },
+  {
+    id: 8,
+    question:'Question 8: this is the content of question 8',
+    answer: 'Answer',
+    options:[
+      'Answer',
+      'Answer 1',
+      'Answer 2',
+      'Answer 3'
+    ]
+  },
+  {
+    id: 9,
+    question:'Question 9: this is the content of question 9',
+    answer: 'Answer',
+    options:[
+      'Answer',
+      'Answer 1',
+      'Answer 2',
+      'Answer 3'
+    ]
+  },
+  {
+    id: 10,
+    question:'Question 10: this is the content of question 10',
     answer: 'Answer',
     options:[
       'Answer',
--- src/component/Flashcard.js
+++ src/component/Flashcard.js
@@ -3,8 +3,13 @@ import React, {useState} from "react";
 export default function Flashcard ({flashcard}){
     const [flip, setFlip] = useState(false)
     return (
-        <div onClick={()=> setFlip(!flip)}>
-            {flip ? flashcard.answer : flashcard.question}
+        <div
+            className={`card ${flip ? 'flip' : ''}`} 
+            onClick={()=> setFlip(!flip)}
+        >
+            <div className="front">{flashcard.question}</div>
+            <div className="back">{flashcard.answer}</div>
+
         </div>
     )
 }
